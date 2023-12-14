706908a12e66d7dc89396e0d3ecf68e56af0098c
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-12-14 21:47:42 +0100

    adds chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import "./App.css";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <div className="App"> Hello world</div>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="pink">
+        Nav
+      </GridItem>
+      <Show above="lg">
+      <GridItem area="aside" bg="skyblue">
+        Aside
+      </GridItem>
+      </Show>
+      <GridItem area="main" bg="gold">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
