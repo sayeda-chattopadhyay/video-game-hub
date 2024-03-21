4f3a5fd9386cada94dd211b5781cc7df28dceff4
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2024-03-21 09:01:46 +0100

    add custom theme color

--- src/theme.tsx
+++ src/theme.tsx
@@ -4,6 +4,22 @@ const config: ThemeConfig = {
   initialColorMode: "dark",
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({
+  config,
+  colors: {
+    gray: {
+      50: "#f7fafc",
+      100: "#edf2f7",
+      200: "#e2e8f0",
+      300: "#cbd5e0",
+      400: "#a0aec0",
+      500: "#718096",
+      600: "#4a5568",
+      700: "#2d3748",
+      800: "#1a202c",
+      900: "#212121",
+    },
+  },
+});
 
 export default theme;
