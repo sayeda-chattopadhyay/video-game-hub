4683b4cbf74276c962cb00c56a69534aa0ce380c
Sayeda Chattopadhyay <sayeda.b@gmail.com>
2026-03-08 21:30:02 +0100

    install react router dom

--- src/hooks/useData.tsx
+++ src/hooks/useData.tsx
@@ -1,50 +1,22 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import apiClient from "../services/api-client";
-import { AxiosRequestConfig, CanceledError } from "axios";
+import { AxiosRequestConfig } from "axios";
 
 interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-const useData = <T,>(
-  endpoint: string,
-  requestConfig?: AxiosRequestConfig,
-  deps?: any[]
-) => {
-  const [data, setData] = useState<T[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
+const useData = <T,>(endpoint: string, requestConfig?: AxiosRequestConfig) => {
+  const { data, error, isLoading } = useQuery<FetchResponse<T>, Error>({
+    queryKey: [endpoint, requestConfig?.params],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<T>>(endpoint, requestConfig)
+        .then((res) => res.data),
+  });
 
-  useEffect(
-    () => {
-      const controller = new AbortController();
-
-      const fetchGames = async () => {
-        try {
-          setLoading(true);
-          const response = await apiClient.get<FetchResponse<T>>(endpoint, {
-            signal: controller.signal,
-            ...requestConfig,
-          });
-          setData(response.data.results);
-          setLoading(false);
-        } catch (error: any) {
-          if (error instanceof CanceledError) return;
-          setError(error.message);
-          setLoading(false);
-        }
-      };
-      fetchGames();
-
-      return () => {
-        controller.abort();
-      };
-    },
-    deps ? [...deps] : []
-  );
-
-  return { data, error, isLoading };
+  return { data: data?.results ?? [], error: error?.message ?? "", isLoading };
 };
 
 export default useData;
